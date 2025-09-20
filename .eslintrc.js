786ad2ac3759ba2586abd1f6643672c666776a6f
Juma <safartyjumah@gmail.com>
2025-09-20 16:47:56 +0300

    chore: update package.json dependencies and format platforms array

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,10 @@ module.exports = {
   root: true,
   extends: ['universe/native', 'universe/web'],
   ignorePatterns: ['build'],
+  rules: {
+    // Relax some rules for publication readiness
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 };
